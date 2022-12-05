32998cb33559ee3a2c744f26de7dd9ca7ac4eb80
Bryce Lam <bcllamc280@gmail.com>
2022-12-05 10:03:09 -0800

    Create Sudoku.asm

---
+++ Sudoku.asm
@@ -0,0 +1,11 @@
+# 12/5/22
+# CS2640 Final Project: Sudoku
+# Written by Avengers Assembly Group
+# Names: Bryce Lam, Colin Mcgough, Evan Cheng, Loc Nguyen
+# Objective: Creating a 4x4 Sudoku game in MIPS that a user can solve
+.include "SudokuMacros.asm"
+
+.data
+
+.text
+
